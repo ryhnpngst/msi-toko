85adeeb7a7e60a0436c3bc3b57fdec303cdf57ee
ryhnpngst <rayhanwibowo24@gmail.com>
2022-12-04 19:22:05 +0700

    Update Name

--- config/cek_login.php
+++ config/cek_login.php
@@ -16,17 +16,23 @@ $data = mysqli_query($koneksi, "SELECT * FROM user WHERE email='$email' and pass
 $cek = mysqli_num_rows($data);
 
 if($cek>0){
-    // Mengambil data nama
-    $ambilNama = mysqli_query($koneksi, "SELECT name FROM user WHERE email = '$email'");
-    $hasilName = mysqli_fetch_assoc($ambilNama);
-    $name = implode($hasilName);
+    // Mengambil data nama depan
+    $getFirstName = mysqli_query($koneksi, "SELECT first_name FROM user WHERE email = '$email'");
+    $resultFirstName = mysqli_fetch_assoc($getFirstName);
+    $firstName = implode($resultFirstName);
+
+    // Mengambil data nama depan
+    $getLastName = mysqli_query($koneksi, "SELECT last_name FROM user WHERE email = '$email'");
+    $resultLastName = mysqli_fetch_assoc($getLastName);
+    $lastName = implode($resultLastName);
+    
 
     // Mengambil data role
     $ambilRole = mysqli_query($koneksi, "SELECT role FROM user WHERE email = '$email'");
     $hasilRole = mysqli_fetch_assoc($ambilRole);
     $role = implode($hasilRole);
     
-    $_SESSION['name'] = $name;
+    $_SESSION['name'] = $firstName." ".$lastName;
     $_SESSION['role'] = $role;
     $_SESSION['status'] = "login";
 
---
+++ pages/pengguna.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengguna</title>
+</head>
+<body>
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+        <!--  -->
+        <?php $page = 'pengguna';?>
+        <?php include "../template/sidebar.php"; ?>
+        <!-- -->
+        <div class="content">
+
+
+        <!--  -->
+        <?php include "../template/footer.php" ?>
+        <!--  -->
+        </div>
+    </div>
+</body>
+</html>
